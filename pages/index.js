656cc39f85e92b470f3e45c410acfd9e5e1f4b20
Jose Rosales <102900240+jrtm96@users.noreply.github.com>
2023-02-22 09:37:37 +0000

    Completed Toast Lesson
    react-hot-toast is a "hot" UI Library for notificatons that apper onscreen and show success/notifications in a very centraized and uniform manner by intalling react-hot-toast using npm command.

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,19 @@ import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from '/styles/Home.module.css';
 import Link from 'next/link';
-
+import toast from 'react-hot-toast';
 
 import Loader from '../components/Loader';
 
 export default function Home() {
   return (
-    <div>
-      <Loader show />
-    </div>
+    <>
+      <div>
+        <Loader show />
+      </div>
+      <button onClick={() => toast.success('hello toast!')}>
+        Toast Me!
+      </button>
+    </>
   );
 }
